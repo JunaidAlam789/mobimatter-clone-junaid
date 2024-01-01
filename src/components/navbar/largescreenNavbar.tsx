c4207278904fdc23fe221fee4d2897d261366d14
SidrahR <57841330+SidrahR@users.noreply.github.com>
2024-01-02 01:14:19 +0500

    navbar completed

---
+++ src/components/navbar/largescreenNavbar.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Logo from "@/components/logo";
+import Link from "next/link";
+import { navbarData } from ".";
+import { ChevronDown } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import DropdownMenu from "./dropdownMenu";
+
+export default function LargescreenNavbar() {
+  return (
+    <div className="flex items-center justify-between max-w-screen-2xl mx-auto px-8 h-16 border-b-4 border-[#F2F6F8] z-[100] relative">
+      {/* Logo */}
+      <Logo />
+      {/* Links and Buttons */}
+      <div className="flex items-center gap-7 h-full">
+        {navbarData.map((item, index) => (
+          <ul
+            key={index}
+            className="flex items-center text-sm h-full duration-300 transition-colors ease-in-out"
+          >
+            <li>
+              {index === 0 ? (
+                <div className="cursor-pointer group h-14 flex items-center">
+                  <span className="hover:text-slate-500 flex items-center">
+                    {item.text}
+                    <ChevronDown className="h-5 w-5 ml-1 hover:text-slate-500" />
+                  </span>
+
+                  {/* Dropdown menu */}
+                  <div className="invisible group-hover:visible -translate-y-3 group-hover:translate-y-0 opacity-0 group-hover:opacity-100 absolute top-16 left-0 w-full transition-all duration-300">
+                    <DropdownMenu />
+                  </div>
+                </div>
+              ) : (
+                <Link className="hover:text-slate-500" href={item.href}>
+                  {item.text}
+                </Link>
+              )}
+            </li>
+          </ul>
+        ))}
+        {/* Button */}
+        <Link href="/signup">
+          <Button className="bg-[#38BDEF] hover:bg-blue-800">Join Now</Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/navbar/mobileNavbar.tsx
@@ -0,0 +1,75 @@
+import { ChevronRight, LogIn, Menu } from "lucide-react";
+import Logo from "@/components/logo";
+import Link from "next/link";
+import Image from "next/image";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { navbarData } from ".";
+import { dropdownMenuData } from ".";
+
+export default function MobileNavbar() {
+  return (
+    <div className="flex items-center justify-between w-full h-16 px-2">
+      {/* SideMenu */}
+      <Sheet>
+        <SheetTrigger>
+          <Menu size={25} />
+        </SheetTrigger>
+        <SheetContent
+          side="left"
+          className="w-full h-screen overflow-y-auto pt-10"
+        >
+          {navbarData.map((item, index) => (
+            <div key={item.text} className="mb-4">
+              {index === 0 ? (
+                <div>
+                  <Sheet>
+                    <SheetTrigger className="flex items-center">
+                      {item.text}
+                      <ChevronRight className="h-4 w-4 ml-1" />
+                    </SheetTrigger>
+                    <SheetContent
+                      side="left"
+                      className="w-full pt-10 h-screen overflow-y-auto"
+                    >
+                      {dropdownMenuData.map((item, index) => (
+                        <Link
+                          key={index}
+                          href={item.href}
+                          className="flex items-start gap-x-3 w-full rounded-lg hover:bg-slate-50 duration-200 ease-in-out transition-colors mb-5 p-2"
+                        >
+                          <Image
+                            src={item.icon}
+                            alt="icon"
+                            width={20}
+                            height={20}
+                            className="rounded-lg mt-1"
+                          />
+                          <div className="space-y-1">
+                            <h2>{item.label}</h2>
+                            <h4 className="text-slate-400">
+                              {item.description}
+                            </h4>
+                          </div>
+                        </Link>
+                      ))}
+                    </SheetContent>
+                  </Sheet>
+                </div>
+              ) : (
+                <Link className="hover:text-slate-500" href={item.href}>
+                  {item.text}
+                </Link>
+              )}
+            </div>
+          ))}
+        </SheetContent>
+      </Sheet>
+      {/* Logo */}
+      <Logo />
+      {/* Login Button */}
+      <Link href="/signup">
+        <LogIn />
+      </Link>
+    </div>
+  );
+}
