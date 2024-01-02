d8d4c2711db1301516efdf28daadb0b6e9076712
Muhammad Mehdi <muhammadmahdi512@gmail.com>
2024-01-02 20:35:23 +0500

    Cards && countries !

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
+import { getCountriesData } from "@/utils/getCountriesdata";
 import { HeroSection } from "@/views/homepage/HeroSection";
+import { PromotionCards } from "@/views/homepage/PromotionCards";
 import { PopularCountries } from "@/views/homepage/popularCountries";
 
-export default function Home() {
+export default async function Home() {
   return (
-    <main className="">
+    <>
       <HeroSection />
       <PopularCountries />
-    </main>
+      <PromotionCards />
+    </>
   );
 }
