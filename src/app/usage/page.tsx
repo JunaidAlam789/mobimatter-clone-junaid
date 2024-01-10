f73cbf4a13f60e57c61bd73386a30ed9189df340
gharis <gharisbinqasim@gmail.com>
2024-01-10 23:03:13 +0500

    adding usage page responsiveness

--- src/app/usage/page.tsx
+++ src/app/usage/page.tsx
@@ -3,16 +3,16 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='min-h-[70vh] min-w-screen mx-[7rem] px-[1.5rem] flex flex-col items-center pt-10 space-y-4'>
+    <div className='min-h-[70vh] min-w-screen lg:mx-[7rem] lg:px-[1.5rem] px-[0.5rem] flex flex-col items-center pt-10 space-y-4'>
       <div className='w-full'>
-        <h2 className='text-[1.8rem] font-bold'>eSIM Status and Usage Check</h2>
+        <h2 className='text-[1.8rem] fontsemi-bold'>eSIM Status and Usage Check</h2>
         <p>You can see your data usage directly from your phone settings.</p>
       </div>
       <div className='w-full'>
         <p>Alternatively, enter your order number:</p>
-        <div className='flex w-full space-x-2 space-y-2'>
-          <input className='w-full rounded-md border-2 border-btnblue outline-none px-6 focus:shadow-sm focus:shadow-btnblue' placeholder='MM-12345'/>
-          <Button className='bg-btnblue hover:bg-hoverbtnblue' size='lg'>
+        <div className='flex flex-col sm:flex-row  w-full sm:space-x-2 space-y-2 sm:space-y-0'>
+          <input className='w-full rounded-md border-2 border-btnblue outline-none px-6 focus:shadow-sm py-2 focus:shadow-btnblue' placeholder='MM-12345'/>
+          <Button className='bg-btnblue hover:bg-hoverbtnblue ' size='lg'>
             Check
           </Button>
         </div>
